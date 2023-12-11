f7cbe0e47b7cdce992e8c5c6ed2d11c1cd99c635
royamousavi <roya2002_rm@yahoo.com>
2023-12-11 14:45:11 +0200

    add gitignor and module andd remove unneccesery file

---
+++ src/webappi/Models/DbroyaContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace webappi.Models;
+
+public partial class DbroyaContext : DbContext
+{
+    public DbroyaContext()
+    {
+    }
+
+    public DbroyaContext(DbContextOptions<DbroyaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:dbroya.database.windows.net,1433;Initial Catalog=dbroya;Persist Security Info=False;User ID=dbroya;Password=Roya2002_rm;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.City)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PersonId).HasColumnName("PersonID");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
